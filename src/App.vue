d9386a608cadfb6e16bdcb0bcdc9d950c0e76c19
David Christian Jones <david@dcj.codes>
2019-08-18 07:00:13 -0400

    Landing Page : Add Map // Add Router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Landing msg="Welcome to the Five Boroughs of NYC" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Landing from "./components/landing/Landing.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Landing
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/landing/Landing.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="landing">
+    <h1>{{ msg }}</h1>
+    <Map />
+  </section>
+</template>
+
+<script>
+import Map from "./borough-map/Map";
+
+export default {
+  name: "Landing",
+  components: {
+    Map
+  },
+  props: {
+    msg: String
+  }
+};
+</script>
